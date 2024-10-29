3d42ae3ada8fbaf4b9185ea356f12f3af6590886
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-10-29 22:56:22 +0530

    Create App Constants

---
+++ app/src/main/java/com/example/vpn/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.vpn.utils
+
+object Constants {
+
+    const val TIMEOUT = 30000L
+    const val API_KEY = "e4e7d86908b2486fa1bdc74cc1418c3d"
+    const val DOWNLOAD = 2.6
+    const val UPLOAD = 3.5
+
+}
